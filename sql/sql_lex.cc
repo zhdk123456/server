4f583eb3552c7f8f9a23f16ec0a19b02603d2ce8
Alexander Barkov <bar@mariadb.org>
2016-09-07 12:27:36 +0400

    Fixed a crash in a EXIT/CONTINUE with an unknown identifier in the WHEN clause
    The crash happened because of a wrong reset_lex() .. restore_lex() sequence.
    The Item in WHERE clause and the corresponding sp_instr_jump_if_not() were
    erroneously created using different LEX.

--- sql/sql_lex.cc
+++ sql/sql_lex.cc
@@ -5340,7 +5340,6 @@ bool LEX::sp_exit_block(THD *thd, sp_label *lab, Item *when)
   if (!when)
     return sp_exit_block(thd, lab);
 
-  sphead->reset_lex(thd); // This changes thd->lex
   DBUG_ASSERT(sphead == thd->lex->sphead);
   DBUG_ASSERT(spcont == thd->lex->spcont);
   sp_instr_jump_if_not *i= new (thd->mem_root)
@@ -5349,7 +5348,6 @@ bool LEX::sp_exit_block(THD *thd, sp_label *lab, Item *when)
                                                 when, thd->lex);
   if (i == NULL ||
       sphead->add_instr(i) ||
-      sphead->restore_lex(thd) ||
       sp_exit_block(thd, lab))
     return true;
   i->backpatch(sphead->instructions(), spcont);
@@ -5415,7 +5413,6 @@ bool LEX::sp_continue_loop(THD *thd, sp_label *lab, Item *when)
   if (!when)
     return sp_continue_loop(thd, lab);
 
-  sphead->reset_lex(thd); // This changes thd->lex
   DBUG_ASSERT(sphead == thd->lex->sphead);
   DBUG_ASSERT(spcont == thd->lex->spcont);
   sp_instr_jump_if_not *i= new (thd->mem_root)
@@ -5424,7 +5421,6 @@ bool LEX::sp_continue_loop(THD *thd, sp_label *lab, Item *when)
                                                 when, thd->lex);
   if (i == NULL ||
       sphead->add_instr(i) ||
-      sphead->restore_lex(thd) ||
       sp_continue_loop(thd, lab))
     return true;
   i->backpatch(sphead->instructions(), spcont);
